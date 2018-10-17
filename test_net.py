7ff7e8ade86dc2d739c94d957ee4b45d1f8bab23
Quan Kong <quankong@Quans-MacBook-Pro.local>
2018-10-17 14:06:24 +0800

    update visual

--- test_net.py
+++ test_net.py
@@ -5,7 +5,7 @@
 # -----------------------------------------------------------------------------
 from config.config_input import Pascal2007Test
 from config.config_model import DefaultModelConfig
-from config.config_train import DefaultTrainConfig
+from config.config_train import DefaultTestConfig
 
 from trainval.processor import Processor
 
@@ -14,10 +14,9 @@ from trainval.processor import Processor
 test_data_cfg = Pascal2007Test()
 
 test_model_cfg = DefaultModelConfig(test_data_cfg)
+test_cfg = DefaultTestConfig()
 
 proc = Processor()
-# weights_file = './tensorboard/default/ckpt/'
-weights_file = './dataset/pretrain_model/VGGnet_fast_rcnn_iter_70000.ckpt'
-proc.test_loop(test_data_cfg, test_model_cfg, weights_file)
+proc.test_loop(test_data_cfg, test_model_cfg, test_cfg)
 
 print('test done')
